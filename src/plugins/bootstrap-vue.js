d3fdddcfc369dfc2d0866748f7e7cf4abc2ccbcd
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-03-12 10:56:29 +0100

    chore: passage a boostrap-vue-next 0.16

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,8 +1,8 @@
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import BootstrapVueNext from 'bootstrap-vue-next'
+import { createBootstrap } from 'bootstrap-vue-next'
 
 export default function (app) {
-	app.use(BootstrapVueNext)
+	app.use(createBootstrap())
 
 	return app
 }
